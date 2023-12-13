9339cf4c18dac1a001979944d8c139caebcd4215
Lahiru Jayakody <lahiiru@gmail.com>
2023-12-13 14:17:31 +0530

    fixes for udith changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,42 @@
 import './App.css'
 // src/App.tsx
-import React from 'react';
+import React, {PropsWithChildren} from 'react';
 import Chat from './components/Chat';
+import {AuthProvider, useAuth} from 'oidc-react';
+import {AuthProviderProps} from "oidc-react/build/src/AuthContextInterface";
+
+const oidcConfig: AuthProviderProps = {
+  onSignIn: (user) => {
+    if (user) {
+      const accessToken = user.access_token;
+      alert(accessToken);
+      // Use the access token as needed
+    }
+  },
+  authority: 'https://idp.syntaxgenie.com/realms/intelliscript',
+  clientId: 'is-chat-app-dev',
+  redirectUri: 'http://localhost:5170',
+  responseType: 'code',
+  scope: 'openid profile email',
+};
+
+const PrivateRoute = (props: PropsWithChildren) => {
+  const { isLoading, userData, signIn } = useAuth();
+  console.log(isLoading, userData, signIn)
+
+  return props.children
+}
+
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <Chat />
-    </div>
+    <AuthProvider {...oidcConfig}>
+      <div className="App">
+        <PrivateRoute>
+          <Chat />
+        </PrivateRoute>
+      </div>
+    </AuthProvider>
   );
 };
 
--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,6 +1,5 @@
-
 // src/components/Chat.tsx
-import React, { useState, useEffect,useCallback } from 'react';
+import React, {useState, useEffect} from 'react';
 
 const apiUrl = 'http://127.0.0.1:8000';
 
@@ -11,24 +10,22 @@ interface ChatMessage {
 
 const Chat: React.FC = () => {
   const [messages, setMessages] = useState<ChatMessage[]>([]);
-  const [newMessage, setNewMessage] = useState<string>('');
+  const [currentMessage, setCurrentMessage] = useState<string>('');
   const [showFeatures, setShowFeatures] = useState<boolean>(true);
-  
-  const handleSendMessage = async () => {
-    if (newMessage.trim() !== '') {
-      await sendMessageToChatbot(newMessage);
 
-      //await fetchGreetMessage();
-      setNewMessage('');
+  const handleSendMessage = async () => {
+    if (currentMessage.trim() !== '') {
+      await sendMessageToServer(currentMessage);
+
+      setCurrentMessage('');
       setShowFeatures(false);
     }
   };
 
-  const sendMessageToChatbot = async (message: string) => {
+  const sendMessageToServer = async (message: string) => {
     try {
       if (message) {
-        setMessages((prevMessages) => [...prevMessages,{role :"user", content : message}]);
-        
+        setMessages((prevMessages) => [...prevMessages, {role: "user", content: message}]);
       }
       const response = await fetch(`${apiUrl}/chatbot`, {
         method: 'POST',
@@ -36,19 +33,15 @@ const Chat: React.FC = () => {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-          messages: [{ role: 'user', content: message }],
+          messages: [{role: 'user', content: message}],
         }),
       });
 
       if (response.ok) {
         const data = await response.json();
-        const reversedMessages = data.messages[0]?.content;
-
-      if (reversedMessages) {
-        // Add reversed messages to the state
-        setMessages((prevMessages) => [...prevMessages, reversedMessages]);
-      }
-
+        if (data.messages) {
+          setMessages((prevMessages) => [...prevMessages, ...data.messages]);
+        }
       } else {
         console.error('Failed to send message to chatbot');
       }
@@ -57,58 +50,46 @@ const Chat: React.FC = () => {
     }
   };
 
-  const fetchGreetMessage = useCallback(async () => {
+  const fetchGreetMessage = async () => {
     try {
       const response = await fetch(`${apiUrl}/greet`);
-  
+
       if (response.ok) {
         const data = await response.json();
-        
-        // Check if greet message already exists in messages
-        const greetMessageExists = messages.some(
-          (message) => message.role === 'chatbot' && message.content === data.message
-        );
-  
-        if (!greetMessageExists) {
-          // Filter out existing greet messages and add the new one
-          setMessages((prevMessages) => [
-            ...prevMessages.filter((message) => message.role !== 'chatbot'),
-            { role: 'chatbot', content: data.message },
-          ]);
-        }
+        // Filter out existing greet messages and add the new one
+        setMessages((prevMessages) => [
+          ...prevMessages.filter((message) => message.role !== 'chatbot'),
+          {role: 'chatbot', content: data.message},
+        ]);
       } else {
         console.error('Failed to fetch chat messages');
       }
     } catch (error) {
       console.error('Error fetching chat messages', error);
     }
-  }, [messages]);
-  
-  
+  };
+
   useEffect(() => {
-    const fetchData = async () => {
-      await fetchGreetMessage();
-    };
-    fetchData();
-  }, [fetchGreetMessage]);
-  
+    fetchGreetMessage()
+  }, []);
+
   return (
     <div className="flex flex-col h-screen bg-gray-100 p-4">
       {/* Top Stripe */}
       <div className="flex items-center justify-between mb-4">
         <div className="flex items-center">
-          <img src="/path/to/intelliscript-logo.png" alt="Intelliscript Logo" className="mr-2" />
-          <span className="text-lg font-semibold">Intelliscript</span>
+          <img src="/path/to/intelliscript-logo.png" alt="IntelliScript Logo" className="mr-2"/>
+          <span className="text-lg font-semibold">IntelliScript</span>
         </div>
         <div className="flex items-center">
           <input
             type="text"
             className="p-2 border rounded-l-md"
             placeholder="Search in Chat..."
-            style={{ height: '32px' }}
+            style={{height: '32px'}}
             maxLength={200}
           />
-          <button className="bg-blue-500 text-white p-2 rounded-r-md"style={{ padding: '4px 12px' }}>Search</button>
+          <button className="bg-blue-500 text-white p-2 rounded-r-md" style={{padding: '4px 12px'}}>Search</button>
         </div>
       </div>
       {/* Bottom Stripe */}
@@ -120,7 +101,7 @@ const Chat: React.FC = () => {
           </div>
           <h2 className="text-lg font-semibold mb-2">Chat History</h2>
           {/* Display chat history here */}
-          {Array.from({ length: 20 }, (_, index) => (
+          {Array.from({length: 20}, (_, index) => (
             <div
               key={index}
               //className={`mb-4 transition-opacity duration-300 ${index >= visibleChats ? 'opacity-0' : 'opacity-100'}`}
@@ -134,7 +115,7 @@ const Chat: React.FC = () => {
             <div>Email: udithw@syntaxgenie.com</div>
           </div>
         </div>
-            {/* Right Side */}
+        {/* Right Side */}
         <div className="flex-1 bg-white rounded-lg p-4 overflow-y-auto relative ">
           {showFeatures && (
             <div className="text-center text-gray-500 mb-4">
@@ -149,42 +130,31 @@ const Chat: React.FC = () => {
             </div>
           )}
           {/* Display Messages */}
-        {messages.map((message, index) => (
-          <div
-            key={index}
-            className={`mb-2 ${
-              message.role === 'user' ? 'bg-yellow-300 text-black' : 'bg-green-300 text-black'
-            }`}
-            style={{
-              borderRadius: '10px',
-              padding: '8px',
-              alignSelf: message.role === 'user' ? 'flex-end' : 'flex-start',
-            }}
-          >
-            {message.role === 'chatbot' ? (
-              typeof message === 'string'
-                ? message // Plain string
-                : message.content // Extract original user message
-            ) : (
-              typeof message === 'string'
-                ? message // Plain string
-                : message.content // Extract reversed text
-            )}
-          </div>
-        ))}
-
-
+          {messages.map((message, index) => (
+            <div
+              key={index}
+              className={`mb-2 ${
+                message.role === 'user' ? 'bg-yellow-300 text-black' : 'bg-green-300 text-black'
+              }`}
+              style={{
+                borderRadius: '10px',
+                padding: '8px',
+                alignSelf: message.role === 'user' ? 'flex-end' : 'flex-start',
+              }}
+            >
+              {message.content}
+            </div>
+          ))}
 
           {/* Message Input */}
           <div className="absolute bottom-4 left-0 right-0">
             <div className="flex items-center justify-end p-2">
-              <input
-                type="text"
-                value={newMessage}
-                onChange={(e) => setNewMessage(e.target.value)}
+              <textarea
+                value={currentMessage}
+                onChange={(e) => setCurrentMessage(e.target.value)}
                 className="fixed bottom-4 flex-1 p-2 border rounded-l-md"
                 placeholder="Type a message..."
-                style={{ width: '70%' }} 
+                style={{width: '70%'}}
               />
               <button
                 onClick={handleSendMessage}
